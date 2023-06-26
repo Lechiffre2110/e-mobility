45055b69a2221075d44bbc848f1eaecfb6717caf
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-06-26 17:58:14 +0200

    add footer component

---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <>
+            <div className=" w-full h-3 flex flex-row bg-slate-300">
+                <h1>Footer</h1>
+            </div>
+        </>
+    );
+}
