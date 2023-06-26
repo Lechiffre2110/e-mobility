e10f62b947ae9bd98b291642d2252f0bc696b074
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-06-26 17:56:49 +0200

    add landing page

---
+++ src/pages/landing-page.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/navbar";
+
+function LandingPage(props) {
+  return (
+    <>
+      <div>
+        <h1>Landing Page</h1>
+      </div>
+    </>
+  );
+}
+export default LandingPage;
