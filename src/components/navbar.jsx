446f51ace2a9a82273159ab13455181ac76572bb
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-06-26 17:58:23 +0200

    add navbar component

---
+++ src/components/navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <>
+            <div className=" w-full h-3 flex flex-row bg-slate-300">
+                <h1>E Mobility</h1>
+            </div>
+        </>
+    );
+}
