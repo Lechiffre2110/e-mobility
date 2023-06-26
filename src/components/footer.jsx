880a4d70230e21317b24c6e75889538720e9830d
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-06-27 00:02:54 +0200

    add imprint and github links

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,12 @@
+import GithubLogo from "../assets/github.svg";
+
 export default function Footer() {
-    return (
-        <>
-            <div className=" w-full h-3 flex flex-row bg-slate-300">
-                <h1>Footer</h1>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="w-full flex flex-row justify-between items-center h-10 px-10 fixed bottom-5">
+        <img className="hover:bg-red-300 rounded-full h-9" src={GithubLogo}></img>
+        <a className="group ease-in duration-300">Impressum</a>
+      </div>
+    </>
+  );
 }
