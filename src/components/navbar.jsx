1724b020aa7ab8882c1c406d5ee1569a7233d723
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-06-27 00:03:07 +0200

    add hover animations

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,20 +7,24 @@ export default function Navbar() {
                 <h1 className="text-center font-extrabold text-3xl">Project E-Mobility</h1>
 
                 <nav className="bg-white w-[60%]">
-                    <ul className="flex flex-row justify-around [&>li]:py-2 [&>li]:px-3">
-                        <li>
+                    <ul className="flex flex-row justify-around [&>li]:py-2 [&>li]:px-5">
+                        <li className="group ease-in duration-300">
                             <Link to="/authors">Autoren</Link>
+                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 green-background"></span>
                         </li>
-                        <li>
+                        <li className="group ease-in duration-300">
                             <Link to="/cars">Fahrzeuge</Link>
+                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 green-background"></span>
                         </li>
-                        <li>
+                        <li className="group ease-in duration-300">
                             <Link to="/documentation">Dokumentation</Link>
+                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 green-background"></span>
                         </li>
-                        <li>
+                        <li className="group ease-in duration-300">
                             <Link to="/blog">Blog</Link>
+                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 green-background"></span>
                         </li>
-                        <li className=" bg-green-300 rounded-lg">
+                        <li className="green-background rounded-full hover:bg-emerald-500">
                             <Link className=" font-semibold" to="/datahub">Data Hub</Link>
                         </li>
                     </ul>
