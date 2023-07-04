bf1bd900245b377b86c2fa7e1a12cf180e0ffc3a
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-07-04 14:23:00 +0200

    design tweaks

--- src/pages/cars.jsx
+++ src/pages/cars.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Dialog } from "primereact/dialog";
 
 import { TabView, TabPanel } from "primereact/tabview";
+import ID3Info from "../components/id3-info";
+import MiraiInfo from "../components/mirai-info";
 import VWID3 from "../assets/vw-id3.png.webp";
 
 export default function Cars() {
@@ -14,7 +16,7 @@ export default function Cars() {
         <TabView>
           <TabPanel header="Volkswagen ID3">
             <button
-              className="mr-3 bg-gray-200 px-4 py-2 rounded-full hover:bg-[#6bdbc1] ease-in duration-300"
+              className="hidden lg:inline-block mr-3 bg-gray-200 px-4 py-2 rounded-full hover:bg-[#6bdbc1] ease-in duration-300"
               onClick={() => setID3Visible(true)}
             >
               Technische Daten
@@ -22,7 +24,7 @@ export default function Cars() {
             <button className="mr-3 bg-gray-200 px-4 py-2 rounded-full hover:bg-[#6bdbc1] ease-in duration-300">
               Setup Guide
             </button>
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col items-center justify-center">
               <img className="w-[80vw]" src={VWID3} />
             </div>
             <Dialog
@@ -31,55 +33,18 @@ export default function Cars() {
               style={{ width: "50vw" }}
               onHide={() => setID3Visible(false)}
             >
-              <table class="table-auto border m-auto mt-10 border-gray-300">
-                <thead>
-                  <tr>
-                    <th class="px-4 py-2 border">Spezifikation</th>
-                    <th class="px-4 py-2 border">Wert</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td class="px-4 py-2 border">Hersteller</td>
-                    <td class="px-4 py-2 border">Volkswagen</td>
-                  </tr>
-                  <tr>
-                    <td class="px-4 py-2 border">Leistung</td>
-                    <td class="px-4 py-2 border">150 kW (204 PS)</td>
-                  </tr>
-                  <tr>
-                    <td class="px-4 py-2 border">Max. Drehmoment</td>
-                    <td class="px-4 py-2 border">310 Nm</td>
-                  </tr>
-                  <tr>
-                    <td class="px-4 py-2 border">Höchstgeschwindigkeit</td>
-                    <td class="px-4 py-2 border">160 km/h</td>
-                  </tr>
-                  <tr>
-                    <td class="px-4 py-2 border">Beschleunigung</td>
-                    <td class="px-4 py-2 border">
-                      0-100 km/h in 7.3 Sekunden
-                    </td>
-                  </tr>
-                  <tr>
-                    <td class="px-4 py-2 border">Reichweite</td>
-                    <td class="px-4 py-2 border">420 km (WLTP)</td>
-                  </tr>
-                  <tr>
-                    <td class="px-4 py-2 border">Batteriekapazität</td>
-                    <td class="px-4 py-2 border">58 kWh</td>
-                  </tr>
-                  <tr>
-                    <td class="px-4 py-2 border">Ladezeit</td>
-                    <td class="px-4 py-2 border">30 Minuten (80%)</td>
-                  </tr>
-                </tbody>
-              </table>
+              <ID3Info />
             </Dialog>
+            <div className="lg:hidden">
+              <h2 className="mt-5 text-2xl font-bold text-center">
+                Technische Daten
+              </h2>
+              <ID3Info />
+            </div>
           </TabPanel>
           <TabPanel header="Toyota Mirai">
             <button
-              className="mr-3 bg-gray-200 px-4 py-2 rounded-full hover:bg-[#6bdbc1] ease-in duration-300"
+              className="hidden lg:inline-block mr-3 bg-gray-200 px-4 py-2 rounded-full hover:bg-[#6bdbc1] ease-in duration-300"
               onClick={() => setMiraiVisible(true)}
             >
               Technische Daten
@@ -87,7 +52,7 @@ export default function Cars() {
             <button className="mr-3 bg-gray-200 px-4 py-2 rounded-full hover:bg-[#6bdbc1] ease-in duration-300">
               Setup Guide
             </button>
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col items-center justify-center">
               <img className="w-[80vw]" src={VWID3} />
             </div>
             <Dialog
@@ -96,49 +61,14 @@ export default function Cars() {
               style={{ width: "50vw" }}
               onHide={() => setMiraiVisible(false)}
             >
-              <table class="table-auto border m-auto mt-10 border-gray-300">
-                <thead>
-                  <tr>
-                    <th class="px-4 py-2 border">Spezifikation</th>
-                    <th class="px-4 py-2 border">Wert</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td class="px-4 py-2 border">Hersteller</td>
-                    <td class="px-4 py-2 border">Toyota</td>
-                  </tr>
-                  <tr>
-                    <td class="px-4 py-2 border">Leistung</td>
-                    <td class="px-4 py-2 border">113 kW (154 PS)</td>
-                  </tr>
-                  <tr>
-                    <td class="px-4 py-2 border">Max. Drehmoment</td>
-                    <td class="px-4 py-2 border">335 Nm</td>
-                  </tr>
-                  <tr>
-                    <td class="px-4 py-2 border">Höchstgeschwindigkeit</td>
-                    <td class="px-4 py-2 border">175 km/h</td>
-                  </tr>
-                  <tr>
-                    <td class="px-4 py-2 border">Beschleunigung</td>
-                    <td class="px-4 py-2 border">8.9 Sekunden</td>
-                  </tr>
-                  <tr>
-                    <td class="px-4 py-2 border">Reichweite</td>
-                    <td class="px-4 py-2 border">650 km (WLTP)</td>
-                  </tr>
-                  <tr>
-                    <td class="px-4 py-2 border">Batteriekapazität</td>
-                    <td class="px-4 py-2 border">5.6 kg</td>
-                  </tr>
-                  <tr>
-                    <td class="px-4 py-2 border">Ladezeit</td>
-                    <td class="px-4 py-2 border">5 Minuten (80%)</td>
-                  </tr>
-                </tbody>
-              </table>
+              <MiraiInfo />
             </Dialog>
+            <div className="lg:hidden">
+              <h2 className="mt-5 text-2xl font-bold text-center">
+                Technische Daten
+              </h2>
+              <MiraiInfo />
+            </div>
           </TabPanel>
         </TabView>
       </div>
