aa58ab81b125c2938fab77227d0bd763c9650795
Miguel LH <miguellh@MacBook-Pro-von-Miguel.local>
2023-07-09 10:56:28 +0200

    authors page

---
+++ src/components/Project.jsx
@@ -0,0 +1,25 @@
+import './projects.css'
+
+export const  Project = ({ project }) => (
+  <div className="project">
+    <h1>{project.name}</h1>
+    <div className="authors">
+      <h2>Autor*innen:</h2>
+      {project.authors.map((author, index) => (
+        <div key={index} className="author">
+          <p>{author.name}</p>
+          <p>{author.email}</p>
+        </div>
+      ))}
+    </div>
+    <div className="professors">
+      <h2>Professor*innen:</h2>
+      {project.professors.map((professor, index) => (
+        <div key={index} className="professor">
+          <p>{professor.name}</p>
+          <p>{professor.email}</p>
+        </div>
+      ))}
+    </div>
+  </div>
+);
