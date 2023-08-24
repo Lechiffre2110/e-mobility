4cb5d11f7e9f7591b6fe2661c4f437042326ed39
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-08-24 14:50:48 +0200

    add sample info section

---
+++ src/components/info-section.jsx
@@ -0,0 +1,29 @@
+export default function infoSection(props) {
+
+    return (
+        <>
+        <div>
+            <h2 className="text-2xl font-bold">Was ist Projekt E-Mobility?</h2>
+            <p>Projekt E-Mobility ist ein Fachbereichsübergreifendes Projekt des Fachbereichs Informatik und des Fachbereichs Fahrzeugtechnik. Unter der Betreuung von Prof. Dr. Alexander Huhn und Prof. Dr. Andreas Severin sammeln und analysieren die Studenten Fahrtdaten von Eletroautos, die im Anschluss ausgewertet werden. 
+            Das Ziel ist, Erkenntnisse über die Rekkuperation bei E-Autos zu sammeln und Ansätze zu entwicklen, diese zu verbessern.
+            Um möglichst viele Daten sammeln zu können, ist der Code und die Bauanleitung für ein Raspberry Pi basiertes System open source verfügbar. <br /> 
+            Detaillierte Informationen zum Projekt und der Roadmap sind im <a>Whitepaper</a> zu finden.
+            </p>
+        </div>
+
+        <div>
+            <h2 className="text-2xl font-bold">Wer kann am Projekt mitwirken?</h2>
+            <p>Primär richtet sich das Projekt an Studierende der HTW Berlin. Als Open Source Projekt steht es aber auch Außenstehenden Personen frei, Vorschläge und Pull Requests für das Projekt einzureichen. 
+            Im <a href="https://www.discord.com">Projekt Discord</a> besteht des Weiteren die Möglichkeit, sich mit den aktiven Studenten und anderen Mitwirkenden auszutauschen. 
+            </p>
+        </div>
+
+        <div>
+            <h2 className="text-2xl font-bold">Wer kann am Projekt mitwirken?</h2>
+            <p>Primär richtet sich das Projekt an Studierende der HTW Berlin. Als Open Source Projekt steht es aber auch Außenstehenden Personen frei, Vorschläge und Pull Requests für das Projekt einzureichen. 
+            Im <a href="https://www.discord.com">Projekt Discord</a> besteht des Weiteren die Möglichkeit, sich mit den aktiven Studenten und anderen Mitwirkenden auszutauschen. 
+            </p>
+        </div>
+        </>
+    )
+}
