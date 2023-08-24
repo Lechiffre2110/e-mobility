61f1eb688ea38070ac770e4ddff3010004b8fe32
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-08-24 14:49:30 +0200

    add contribution application page

---
+++ src/components/contribution.jsx
@@ -0,0 +1,192 @@
+import { useState } from "react";
+import { Button } from "primereact/button";
+import * as Form from "@radix-ui/react-form";
+import DropdownMenu from "./dropdown-menu";
+
+export default function Contribution() {
+
+    const rollen = [
+        { name: "Professor", value: "professor" },
+        { name: "Student (HTW)", value: "student" },
+        { name: "Extern", value: "other" },
+    ];
+
+    const [selectedRolle, setSelectedRolle] = useState("");
+    
+    function submitForm(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        formData.append("role", selectedRolle);
+        const res = fetch("http://localhost:5555/api/add", {
+            method: "POST",
+            body: formData,
+        });
+        console.log(res);
+    }
+    
+  return (
+    <>
+      <h2 className="h-16 text-2xl bg-white w-[97%] ml-[2%] rounded-2xl flex items-center px-5 text-gray-700 font-bold">
+        Mitwirkung beantragen
+      </h2>
+      <p className="my-6 text-center">
+        Hier haben Sie die Möglichkeit, sich für eine Mitwirkung am Projekt zu bewerben <br />
+        Dies können Sie entweder als Student der HTW Berlin oder als externer Mitwirkender tun, falls Sie einen Beitrag zum Projekt geleistet haben
+      </p>
+
+      <Form.Root
+          className="w-[97%] ml-[2%] flex flex-row justify-around bg-white rounded-2xl py-5 mt-3 h-auto"
+          onSubmit={submitForm}
+        >
+          <div className="flex flex-col w-[40%]">
+            <Form.Field className="grid mb-[10px]" name="email">
+              <div className="flex items-baseline justify-between">
+                <Form.Label className="text-[15px] font-medium leading-[35px] text-gray-700">
+                  Email
+                </Form.Label>
+                <Form.Message
+                  className="text-[13px] text-gray-700 opacity-[0.8]"
+                  match="valueMissing"
+                >
+                  Bitte geben Sie eine Email an
+                </Form.Message>
+                <Form.Message
+                  className="text-[13px] text-gray-700 opacity-[0.8]"
+                  match="typeMismatch"
+                >
+                  Bitte geben Sie eine valide Email an
+                </Form.Message>
+              </div>
+
+              <Form.Control asChild>
+                <input
+                  className="box-border w-full inline-flex h-[35px] items-center justify-center rounded-[8px] px-[10px] text-[15px] leading-none text-gray outline-none hover:border-gray-400 focus:border-gray-500 bg-[#f6f6f6]"
+                  type="email"
+                  required
+                  placeholder="Gib deine Email Adresse ein"
+                />
+              </Form.Control>
+            </Form.Field>
+
+            <Form.Field className="grid mb-[10px]" name="name">
+              <div className="flex items-baseline justify-between">
+                <Form.Label className="text-[15px] font-medium leading-[35px] text-gray-700">
+                  Name
+                </Form.Label>
+                <Form.Message
+                  className="text-[13px] text-gray-700 opacity-[0.8]"
+                  match="valueMissing"
+                >
+                  Bitte gib einen Namen an
+                </Form.Message>
+                <Form.Message
+                  className="text-[13px] text-gray-700 opacity-[0.8]"
+                  match="typeMismatch"
+                >
+                  Bitte gib einen gültigen Namen an
+                </Form.Message>
+              </div>
+
+              <Form.Control asChild>
+                <input
+                  className="box-border w-full inline-flex h-[35px] items-center justify-center rounded-[8px] px-[10px] text-[15px] leading-none text-gray outline-none hover:border-gray-400 focus:border-gray-500 bg-[#f6f6f6]"
+                  type="text"
+                  required
+                  placeholder="Gib deinen Namen ein"
+                />
+              </Form.Control>
+            </Form.Field>
+
+            <Form.Field className="grid mb-[10px]" name="description">
+              <div className="flex items-baseline justify-between">
+                <Form.Label className="text-[15px] font-medium leading-[35px] text-gray-700">
+                  Beschreibung
+                </Form.Label>
+                <Form.Message
+                  className="text-[13px] text-gray-700 opacity-[0.8]"
+                  match="valueMissing"
+                >
+                  Bitte beschreibe deine Tätigkeit im Projekt
+                </Form.Message>
+              </div>
+              <Form.Control asChild>
+                <textarea
+                  className="box-border w-full inline-flex appearance-none items-center justify-center rounded-[8px] p-[10px] text-[15px] leading-none hover:border-gray-400 focus:border-gray-500 bg-[#f6f6f6] outline-none resize-none"
+                  required
+                  rows="4"
+                  placeholder="Bitte beschreibe deine Tätigkeit im Projekt"
+                />
+              </Form.Control>
+            </Form.Field>
+          </div>
+
+          <div className="flex flex-col w-[40%]">
+            <Form.Field className="grid mb-[10px]" name="name">
+              <div className="flex items-baseline justify-between">
+                <Form.Label className="text-[15px] font-medium leading-[35px] text-gray-700">
+                  Rolle
+                </Form.Label>
+                <Form.Message
+                  className="text-[13px] text-gray-700 opacity-[0.8]"
+                  match="valueMissing"
+                >
+                  Bitte gib deine Rolle an
+                </Form.Message>
+              </div>
+
+              <Form.Control asChild>
+                <DropdownMenu
+                  label="Rollen"
+                  description="Wähle eine Rolle aus"
+                  data={rollen}
+                  onChange={(value) => setSelectedRolle(value)}
+                />
+              </Form.Control>
+            </Form.Field>
+            
+            <Form.Field className="grid mb-[10px]" name="linkedin">
+              <div className="flex items-baseline justify-between">
+                <Form.Label className="text-[15px] font-medium leading-[35px] text-gray-700">
+                  LinkedIn
+                </Form.Label>
+              </div>
+
+              <Form.Control asChild>
+                <input
+                  className="box-border w-full inline-flex h-[35px] items-center justify-center rounded-[8px] px-[10px] text-[15px] leading-none text-gray outline-none hover:border-gray-400 focus:border-gray-500 bg-[#f6f6f6]"
+                  type="text"
+                  placeholder="LinkedIn Link (optional)"
+                />
+              </Form.Control>
+            </Form.Field>
+
+            <Form.Field className="grid mb-[10px]" name="github">
+              <div className="flex items-baseline justify-between">
+                <Form.Label className="text-[15px] font-medium leading-[35px] text-gray-700">
+                  Github
+                </Form.Label>
+              </div>
+
+              <Form.Control asChild>
+                <input
+                  className="box-border w-full inline-flex h-[35px] items-center justify-center rounded-[8px] px-[10px] text-[15px] leading-none text-gray outline-none hover:border-gray-400 focus:border-gray-500 bg-[#f6f6f6]"
+                  type="text"
+                  placeholder="Github Link (optional)"
+                />
+              </Form.Control>
+            </Form.Field>
+            
+            <Form.Submit asChild>
+              <div className="flex flex-row items-center justify-center mt-2">
+                <Button
+                  className="w-[50%] m-auto z-10"
+                  label="Absenden"
+                  rounded
+                />
+              </div>
+            </Form.Submit>
+          </div>
+        </Form.Root>
+    </>
+  );
+}
