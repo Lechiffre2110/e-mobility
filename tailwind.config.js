dcb90d712a6fb080d237ec67983d3c267b2f351d
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-08-24 14:47:49 +0200

    add radix colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,37 @@
+const { blackA, mauve, violet, green } = require('@radix-ui/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        hide: {
+          from: { opacity: 1 },
+          to: { opacity: 0 },
+        },
+        slideIn: {
+          from: { transform: 'translateX(calc(100% + var(--viewport-padding)))' },
+          to: { transform: 'translateX(0)' },
+        },
+        swipeOut: {
+          from: { transform: 'translateX(var(--radix-toast-swipe-end-x))' },
+          to: { transform: 'translateX(calc(100% + var(--viewport-padding)))' },
+        },
+      },
+      animation: {
+        hide: 'hide 100ms ease-in',
+        slideIn: 'slideIn 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        swipeOut: 'swipeOut 100ms ease-out',
+      },
+      colors: {
+        ...blackA,
+        ...mauve,
+        ...violet,
+        ...green,
+      },
+    },
   },
-  plugins: [],
 }
