bd6fac6165c495bd1fbd6afaa3e9cec136d1edd9
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-08-28 00:14:26 +0200

    add dashboard stats

---
+++ src/components/dashboard-stat.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import * as Progress from "@radix-ui/react-progress";
+export default function DashboardStat(props) {
+  const [progress, setProgress] = useState(10);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setProgress(80), props.order * 200);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      <div className="mt-4 w-[200px] h-[90px] flex flex-col justify-around bg-white border rounded-xl px-3 py-1">
+        <div className="flex flex-row items-start justify-between w-full">
+          <img src={props.icon} className="w-[30px] h-[30px]" />
+          <div>
+            <h3>{props.title}</h3>
+            <p className="text-2xl font-bold text-right">{props.value}</p>
+          </div>
+        </div>
+        <Progress.Root
+          className="relative overflow-hidden bg-blackA9 rounded-full w-full h-[5px]"
+          style={{
+            transform: "translateZ(0)",
+          }}
+          value={progress}
+        >
+          <Progress.Indicator
+            className="bg-blue7 w-full h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]"
+            style={{ transform: `translateX(-${100 - progress}%)` }}
+          />
+        </Progress.Root>
+      </div>
+    </>
+  );
+}
--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react";
+import BugDialog from "./bug-dialog";
+import ContributionDialog from "./contribution-dialog";
+import HorizontalSeparator from "./horizontal-separator";
+import DashboardStat from "./dashboard-stat";
+import UserIcon from "../assets/users.svg";
+import BugIcon from "../assets/alert-triangle.svg";
+import DataIcon from "../assets/file.svg";
+import RequestIcon from "../assets/git-branch.svg";
+import DataTable from "./data-table";
+
 export default function Dashboard() {
+  const [bugs, setBugs] = useState([]);
+  const [requests, setRequests] = useState([]);
   const sampleContributors = [
     {
       name: "Max Mustermann",
@@ -26,41 +39,112 @@ export default function Dashboard() {
     },
   ];
 
+  const getBugs = async () => {
+    const res = await fetch("http://localhost:5555/bug/get");
+    const data = await res.json();
+    setBugs(data.data);
+  };
+
+  const getRequests = async () => {
+    const res = await fetch("http://localhost:5555/api/get");
+    const data = await res.json();
+    setRequests(data.data);
+  };
+
+  useEffect(() => {
+    getBugs();
+    getRequests();
+  }, []);
+
   return (
     <>
-      <h2 className="h-16 text-2xl bg-white w-[97%] ml-[2%] rounded-2xl flex items-center px-5 text-gray-700 font-bold">
+      <h2 className="h-16 text-2xl bg-white w-[97%] ml-[2%] rounded-lg flex items-center px-5 text-gray-700 font-bold border">
         Dashboard
       </h2>
 
-      <div className="w-[97%] ml-[2%] flex flex-row justify-around bg-white rounded-2xl py-5 mt-3 h-auto">
-        <div>
-          <h2 className="text-2xl font-bold">Contributors</h2>
-          {sampleContributors.map((contributor) => {
-            return (
-              <div className="flex flex-row justify-between">
-                <p>{contributor.name}</p>
-                <p>{contributor.role}</p>
-              </div>
-            );
-          })}
-        </div>
+      <div className="flex flex-row gap-3 ml-[2%]">
+        <DashboardStat
+          title="Mitwirkende"
+          value="100"
+          icon={UserIcon}
+          order="1"
+        />
+        <DashboardStat
+          title="Datensätze"
+          value="10"
+          icon={DataIcon}
+          order="2"
+        />
+        <DashboardStat title="Bugs" value="4" icon={BugIcon} order="3" />
+        <DashboardStat
+          title="Anfragen"
+          value="2"
+          icon={RequestIcon}
+          order="4"
+        />
+      </div>
 
-        <div>
-          <h2 className="text-2xl font-bold">Requests</h2>
-          <div className="flex flex-row justify-between">
-            {sampleContributors.map((contributor) => {
-              return (
-                <div className="flex flex-col justify-between">
-                  <p>{contributor.name}</p>
-                  <p>{contributor.role}</p>
-                  <p>{contributor.description}</p>
-                  <button>Accept</button>
-                    <button>Decline</button>
-                </div>
-              );
-            })}
+      <div className="grid grid-cols-3 gap-2 ml-[2%]">
+        <div className="flex flex-row justify-around h-auto py-5 mt-3 text-[#333] bg-white border rounded-xl">
+          <div className="w-full px-5">
+            <h2 className="mb-4 font-bold text-gray-500 text-md">Requests</h2>
+            <div className="flex flex-col justify-between w-full">
+              {requests.map((request) => {
+                return (
+                  <div>
+                    <div className="flex flex-row justify-between w-full py-2 text-[14px] font-semibold items-center">
+                      <div className="flex">
+                        <h3>{request.name} -</h3>
+                        <h3 className="ml-1 text-gray-500">{request.role}</h3>
+                      </div>
+                      <ContributionDialog
+                        name={request.name}
+                        mail={request.email}
+                        role={request.role}
+                        description={request.description}
+                      />
+                    </div>
+                    <HorizontalSeparator />
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
+
+        <div className="ml-[2%] flex flex-row justify-around bg-white rounded-xl py-5 px-5 mt-3 h-auto">
+          <div className="w-full">
+            <h2 className="mb-4 font-bold text-gray-500 text-md">Bugs</h2>
+            <div className="flex flex-col text-[14px] font-semibold items-center">
+              {bugs.map((bug) => {
+                return (
+                  <div className="w-full">
+                    <div className="flex flex-row justify-between py-2">
+                      <p className="max-w-[10%] overflow-hidden whitespace-nowrap overflow-ellipsis">
+                        {bug.title}
+                      </p>
+                      <div>
+                        <BugDialog
+                          title={bug.title}
+                          model={bug.model}
+                          description={bug.description}
+                          contact={bug.contact}
+                        />
+                        <button className="ml-2 bg-green4 text-green11 hover:bg-green5 focus:shadow-green7 inline-flex h-[30px] items-center justify-center rounded-md px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none">
+                          Resolve
+                        </button>
+                      </div>
+                    </div>
+                    <HorizontalSeparator />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-4">
+        <DataTable isAdmin={true} />
       </div>
     </>
   );
