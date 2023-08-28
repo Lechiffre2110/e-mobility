260dd3b82d4deffc8f736457b41a0657ac0c2baa
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-08-28 02:05:05 +0200

    ui tweaks

--- src/components/bug-dialog.jsx
+++ src/components/bug-dialog.jsx
@@ -1,5 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { Cross2Icon } from "@radix-ui/react-icons";
+import HorizontalSeparator from "./horizontal-separator";
 
 export default function bugDialog(props) {
   return (
@@ -19,50 +20,54 @@ export default function bugDialog(props) {
             <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
               Review a bug and mark it as resolved, after fixing it.
             </Dialog.Description>
-            <fieldset className="mb-[15px] flex gap-5">
+            <fieldset className="flex flex-col">
               <label
-                className="text-mauve12 w-[90px] text-right text-[15px]"
+                className="text-mauve11 text-[12px]"
                 htmlFor="title"
               >
-                Titel:
+                TITEL
               </label>
               <p
-                className="text-mauve12 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 text-[15px]"
+                className="text-mauve12 focus:shadow-violet8 inline-flex h-auto w-full flex-1 text-[15px]"
               >{props.title}</p>
             </fieldset>
-            <fieldset className="mb-[15px] flex gap-5">
+            <HorizontalSeparator />
+            <fieldset className="flex flex-col">
               <label
-                className="text-mauve12 w-[90px] text-right text-[15px]"
+                className="text-mauve11 text-[12px]"
                 htmlFor="model"
               >
-                Fahrzeug:
+                FAHRZEUG
               </label>
               <p
                 className="text-mauve12 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 text-[15px]"
               >{props.model}</p>
             </fieldset>
-            <fieldset className="mb-[15px] flex gap-5">
+            <HorizontalSeparator />
+            <fieldset className="flex flex-col">
               <label
-                className="text-mauve12 w-[90px] text-right text-[15px]"
+                className="text-mauve11 text-[12px]"
                 htmlFor="description"
               >
-                Beschreibung:
+                BESCHREIBUNG
               </label>
               <p
                 className="text-mauve12 focus:shadow-violet8 inline-flex h-auto w-full flex-1 items-center justify-center text-[15px]"
               >{props.description}</p>
             </fieldset>
-            <fieldset className="mb-[15px] flex gap-5">
+            <HorizontalSeparator />
+            <fieldset className="flex flex-col">
               <label
-                className="text-mauve12 w-[90px] text-right text-[15px]"
+                className="text-mauve11 text-[12px]"
                 htmlFor="contact"
               >
-                Kontakt:
+                KONTAKT
               </label>
               <p
                 className="text-mauve12 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 text-[15px]"
               >{props.contact}</p>
             </fieldset>
+            <HorizontalSeparator />
             <div className="mt-[25px] flex justify-end">
               <Dialog.Close asChild>
                 <button className="bg-green4 text-green11 hover:bg-green5 focus:shadow-green7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none">
