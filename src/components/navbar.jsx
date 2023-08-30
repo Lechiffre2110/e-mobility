4df1a330eb1633a1cb75c2f41bf8f56c5f6e407d
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-08-30 18:05:51 +0200

    fix mobile nav

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,22 +3,27 @@ import { Link } from "react-router-dom";
 import Logo from "../assets/logo.svg";
 
 export default function Navbar() {
-  const [menuOpen, setMenuOpen] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   function toggleMobileMenu() {
-    const menu = document.querySelector("nav");
-    menu.classList.toggle("hidden");
     menuOpen ? setMenuOpen(false) : setMenuOpen(true);
+  }
+
+  function handleLinkClick() {
+    setMenuOpen(false);
   }
 
   return (
     <>
-      <div id="navbar" className="z-[20] h-[4rem] fixed flex flex-col justify-between w-full px-5 pt-5 lg:px-5 lg:flex-row bg-white">
+      <div
+        id="navbar"
+        className="z-[20] h-[4rem] fixed flex flex-col justify-between w-full px-5 pt-5 lg:px-5 lg:flex-row bg-white"
+      >
         <div className="flex gap-2">
-        <img src={Logo} className="hidden w-8 h-8 lg:block" />
-        <h1 className="hidden pb-3 text-2xl font-extrabold lg:block lg:pb-0 lg:text-center">
-          Project E-Mobility
-        </h1>
+          <img src={Logo} className="hidden w-8 h-8 lg:block" />
+          <h1 className="hidden pb-3 text-2xl font-extrabold lg:block lg:pb-0 lg:text-center">
+            Project E-Mobility
+          </h1>
         </div>
         <img src={Logo} className="w-8 h-8 lg:hidden" />
 
@@ -60,36 +65,41 @@ export default function Navbar() {
           </button>
         </div>
 
-        <nav className="pl-5 pt-10 lg:pt-0 border-b-4 border-[#6bdbc1] lg:border-none  fixed lg:block lg:relative w-full pb-5 lg:w-[60%] z-[20] top-14 left-0 lg:top-0 bg-white">
-            <ul className="flex flex-col gap-6 lg:gap-0 lg:flex-row justify-around [&>li]:py-2 lg:[&>li]:px-5 ">
-                <li className="duration-300 ease-in w-min group">
-                  <Link to="/authors">Autoren</Link>
-                  <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 green-background"></span>
-                </li>
-                <li className="duration-300 ease-in w-min group">
-                  <Link to="/cars">Fahrzeuge</Link>
-                  <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 green-background"></span>
-                </li>
-                <li className="duration-300 ease-in w-min group">
-                  <Link
-                    to="https://project-e-mobility.gitbook.io/projekt-e-mobility/"
-                    target="_blank"
-                  >
-                    Dokumentation
-                  </Link>
-                  <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 green-background"></span>
-                </li>
-                <li className="duration-300 ease-in w-min group">
-                  <Link to="/blog">Blog</Link>
-                  <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 green-background"></span>
-                </li>
-                <li className="px-5 rounded-full w-max green-background hover:bg-emerald-500">
-                  <Link className="font-semibold " to="/datahub">
-                    Data Hub
-                  </Link>
-                </li>
-            </ul>
-          </nav>
+        <nav
+          className={`pl-5 pt-10 lg:pt-0 border-b-4 border-[#6bdbc1] lg:border-none ${
+            menuOpen ? "" : "hidden"
+          } lg:block lg:relative w-full pb-5 lg:w-[60%] z-[20] top-14 left-0 lg:top-0 bg-white`}
+        >
+          <ul className="flex flex-col gap-6 lg:gap-0 lg:flex-row justify-around [&>li]:py-2 lg:[&>li]:px-5 ">
+            <li className="duration-300 ease-in w-min group">
+              <Link to="/authors" onClick={handleLinkClick}>Autoren</Link>
+              <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 green-background"></span>
+            </li>
+            <li className="duration-300 ease-in w-min group">
+              <Link to="/cars" onClick={handleLinkClick}>Fahrzeuge</Link>
+              <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 green-background"></span>
+            </li>
+            <li className="duration-300 ease-in w-min group">
+              <Link
+                to="https://project-e-mobility.gitbook.io/projekt-e-mobility/"
+                target="_blank"
+                onClick={handleLinkClick}
+              >
+                Dokumentation
+              </Link>
+              <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 green-background"></span>
+            </li>
+            <li className="duration-300 ease-in w-min group">
+              <Link to="/blog" onClick={handleLinkClick}>Blog</Link>
+              <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 green-background"></span>
+            </li>
+            <li className="px-5 rounded-full w-max green-background hover:bg-emerald-500">
+              <Link className="font-semibold " to="/datahub" onClick={handleLinkClick}>
+                Data Hub
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </>
   );
