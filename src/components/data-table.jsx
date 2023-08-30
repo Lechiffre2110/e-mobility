0b9c85e180a825dd75d744a989fa6cfafa172b89
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-08-30 14:11:46 +0200

    refactor table action button design

--- src/components/data-table.jsx
+++ src/components/data-table.jsx
@@ -150,7 +150,7 @@ export default function DataTable(props) {
                     <td className="px-5 py-3">{data.description}</td>
                     <td className="px-5 py-3">
                       <button
-                        className="px-[10px] text-white bg-violet9 flex-shrink-0 flex-grow-0 basis-auto h-[25px] rounded inline-flex text-[13px] leading-none items-center justify-center outline-none hover:bg-violet10 focus:relative focus:shadow-[0_0_0_2px] focus:shadow-violet7"
+                        className="bg-blue4 text-blue11 hover:bg-blue5 focus:shadow-blue7 inline-flex h-[30px] items-center justify-center rounded-md px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none text-[14px]"
                         onClick={() => downloadFile(data.id)}
                       >
                         Herunterladen
@@ -159,7 +159,7 @@ export default function DataTable(props) {
                     {props.isAdmin && (
                       <td className="px-5 py-3">
                         <button
-                          className="px-[10px] text-white bg-violet9 flex-shrink-0 flex-grow-0 basis-auto h-[25px] rounded inline-flex text-[13px] leading-none items-center justify-center outline-none hover:bg-violet10 focus:relative focus:shadow-[0_0_0_2px] focus:shadow-violet7"
+                          className="bg-red4 text-red11 hover:bg-red5 focus:shadow-red7 inline-flex h-[30px] items-center justify-center rounded-md px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none text-[14px]"
                           onClick={() => props.deleteData(data.id)}
                         >
                           Löschen
