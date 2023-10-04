327640086d28fc0689c101f3c05bcd9b6c8b1a6a
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-10-04 16:12:54 +0200

    ui tweaks

--- src/components/logout-button.jsx
+++ src/components/logout-button.jsx
@@ -4,8 +4,8 @@ const LogoutButton = () => {
     const { logout } = useAuth0();
   
     return (
-      <button className="bg-gray-200 px-4 py-2 rounded-full hover:bg-[#6bdbc1] ease-in duration-300" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-        Log Out
+      <button className="bg-white text-red11  px-4 py-2 rounded-xl lg:hover:bg-[#6bdbc1] ease-in duration-300" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+        Ausloggen
       </button>
     );
   };
