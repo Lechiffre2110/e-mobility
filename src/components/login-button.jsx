d4992c6072d730965ffb1158c6c74b7a36eba431
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-10-05 10:21:48 +0200

    design tweaks

--- src/components/login-button.jsx
+++ src/components/login-button.jsx
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button className="bg-white text-green11 px-4 py-2 rounded-xl lg:hover:bg-[#6bdbc1] ease-in duration-300" onClick={() => loginWithRedirect()}>Einloggen</button>;
+  return <button className="bg-green4 text-green11 hover:bg-green5 focus:shadow-green7 inline-flex h-[30px] items-center justify-center rounded-md px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none text-[14px]" onClick={() => loginWithRedirect()}>Einloggen</button>;
 };
 
 export default LoginButton;
--- src/components/logout-button.jsx
+++ src/components/logout-button.jsx
@@ -4,7 +4,7 @@ const LogoutButton = () => {
     const { logout } = useAuth0();
   
     return (
-      <button className="bg-white text-red11  px-4 py-2 rounded-xl lg:hover:bg-[#6bdbc1] ease-in duration-300" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+      <button className="bg-red4 text-red11 hover:bg-red5 focus:shadow-red7 inline-flex h-[30px] items-center justify-center rounded-md px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none text-[14px]" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
         Ausloggen
       </button>
     );
