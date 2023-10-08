bf4b00e18633daa5bc91cd804765bc150bddfb3c
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-10-08 21:27:57 +0200

    ui tweaks

--- src/pages/datahub.jsx
+++ src/pages/datahub.jsx
@@ -44,7 +44,7 @@ export default function DataHub({t}) {
 
   return (
     <div className="flex flex-col-reverse pb-5 lg:p-5 lg:bg-gray-100 sm:flex-row rounded-xl">
-      <div className="flex flex-col lg:rounded-xl lg:h-[85vh] fixed lg:relative bottom-0 left-0 lg:w-[25%] lg:max-w-[300px] lg:bg-white p-5 text-gray-500 bg-white">
+      <div className="flex flex-col lg:rounded-xl lg:h-[85vh] fixed lg:relative bottom-0 left-0 lg:w-[25%] lg:max-w-[300px] lg:min-h-[650px] lg:bg-white p-5 text-gray-500 bg-white">
         <div className="hidden gap-1 lg:flex lg:flex-col">
           <div className="hidden gap-4 mb-5 lg:flex lg:flex-row">
             <img className="h-8" src={HubLogo} />
@@ -164,7 +164,7 @@ export default function DataHub({t}) {
           </div>
         </div>
       </div>
-      <div className="w-full lg:w-[75%] mb-14 sm:mb-0">
+      <div className="w-full mb-14 sm:mb-0">
         {searchParams.get("menuPage") === "dashboard" && <Dashboard t={t} />}
         {searchParams.get("menuPage") === "quickactions" && <QuickActions t={t} />}
         {searchParams.get("menuPage") === "onboarding" && <Onboarding />}
