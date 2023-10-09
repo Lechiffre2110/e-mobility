b9e3f6de3301e80f445d4508ee31165057c2dd37
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-10-09 09:18:13 +0200

    fix whitespace bug

--- src/components/quick-actions.jsx
+++ src/components/quick-actions.jsx
@@ -195,18 +195,16 @@ function UploadInviteForm({ open, setOpen, submitForm }) {
   const [description, setDescription] = useState("");
 
   useEffect(() => {
-    setLink(
-      `http://localhost:5173/datahub?menuPage=upload&email=${email}&name=${name}&description=${description}`
-    );
+    //set link and replace spaces with %20
+    let link = `http://localhost:5173/datahub?menuPage=upload&email=${email}&name=${name}&description=${description}`;
+    link = link.replace(/ /g, "%20");
+    setLink(link);
   }, [email, name, description]);
 
   async function submitForm(event) {
     const BASE_URL = "http://localhost:5555/api";
     event.preventDefault();
     
-    //replace spaces with %20
-    let link = `http://localhost:5173/datahub?menuPage=upload&email=${email}&name=${name}&description=${description}`;
-    link = link.replace(/ /g, "%20"); 
     const res = await axios.post(`${BASE_URL}/quickactions/invite`, {
       email: email,
       name: name,
